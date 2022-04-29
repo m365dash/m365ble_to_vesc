c54b88dc5f847d102869ec171a7842c24e3549eb
1zuna <marco@ccbluex.net>
2022-04-29 17:35:54 +0200

    test

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,9 @@
 // M365 BLE serial: RX = digital pin 8, TX = digital pin 9
 SoftwareSerial serialBle(8, 9);
 
+int batteryPin = A0;
+int pwrPin = A1;
+
 // VESC serial: RX = digital pin 10, TX = digital pin 11
 SoftwareSerial serialVesc(10, 11);
 
@@ -27,76 +30,76 @@ void setup() {
 
     serialBle.begin(115200); // M365 BLE on 115200 BAUD RATE
     serialVesc.begin(19200); // VESC on 19200 BAUD RATE
+
+    pinMode(batteryPin, OUTPUT);
+    pinMode(pwrPin, OUTPUT);
 }
 
+bool turned_on;
+
 void loop(void) {
+    digitalWrite(batteryPin, HIGH);   
+    digitalWrite(pwrPin, HIGH);
+
     // M365 BLE to VESC
 
-    dash_pack.faultcode = random(1, 99);
-    dash_pack.speed = random(0, 99); // testing speed
-    switch (random(0, 3)) {
-        case 0:
-            dash_pack.mode = M365_MODE_SLOW;
-            break;
-        case 1:
-            dash_pack.mode = M365_MODE_DRIVE;
-            break;
-        case 2:
-            dash_pack.mode = M365_MODE_SPORT;
-            break;
-        default:
-            break;
-    }
-
-    dash_pack.light = random(0, 2);
-    dash_pack.battery = random(100);
-    dash_pack.beep = 0;
-    
-    addCRC((uint8_t*)&dash_pack, dash_pack.len+6);
-    serialBle.write((uint8_t*)&dash_pack, sizeof(dash_pack));
-
-    delay(1000);
-
-    // TODO: wait for RX/TX data bus components to arrive
-
     serialBle.listen();
-    Serial.println(serialBle.available());
 
+    
+
+    // int valueAnalogPin = analogRead(pwrPin);
+    // valueAnalogPin = analogRead(pwrPin);
+    // if (valueAnalogPin > 0) {
+    //      Serial.println((int)valueAnalogPin);
+    // }
+    
     while (serialBle.available() > 0) {
         uint8_t data = (uint8_t)serialBle.read();
         
         if (ninebot_parse(data, &frame) == 0) {
-            Serial.println("code 1");
-            Serial.println(frame.cmd);
-
             switch(frame.cmd){
 				case 0x64: // request of controller data
-                    Serial.println("code crc");
+                    // dash_pack.faultcode = random(1, 99);
+                    dash_pack.speed = random(0, 99); // testing speed
+                    // switch (random(0, 3)) {
+                    //     case 0:
+                    //         dash_pack.mode = M365_MODE_SLOW;
+                    //         break;
+                    //     case 1:
+                    //         dash_pack.mode = M365_MODE_DRIVE;
+                    //         break;
+                    //     case 2:
+                    //         dash_pack.mode = M365_MODE_SPORT;
+                    //         break;
+                    //     default:
+                    //         break;
+                    // }
 
-					// addCRC((uint8_t*)&dash_pack, dash_pack.len+6);
-                    // serialBle.write((uint8_t*)&dash_pack, sizeof(dash_pack));
+                    // dash_pack.light = random(0, 2);
+                    // dash_pack.battery = random(100);
+                    // dash_pack.beep = 0;
+
+					addCRC((uint8_t*)&dash_pack, dash_pack.len+6);
+                    serialBle.write((uint8_t*)&dash_pack, sizeof(dash_pack));
 				    break;
 				case 0x65: // Throttle and brake data
-                    Serial.println("code adc");
-
 					adc1 = frame.payload[1];
 					adc2 = frame.payload[2];
-                    Serial.println(adc1);
-                    Serial.println(adc2);
+                    Serial.println("adc " + String(adc1, HEX) + " adc2 " + String(adc2, HEX));
 
 					// VescToSTM_timeout_reset();
 					// app_check_timer();
 				    break;
+                default:
+                    // todo: implement cmd handling for app connection, not very important, but some might would like to use.
+
+                    Serial.println("unknown cmd: " + String(frame.cmd, HEX));
+                    break;
 			}
         }
     }
 
-    delay(1000);
-
-    
-
-    
-
+    delay(1);
     // VESC data to M365 BLE
 
     
